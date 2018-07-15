2fd497ba4b57a7b75df2d2ed6c62990121e3d286
A. Torres Sampaio <a.torres.sampaio@avanade.com>
2018-07-15 20:09:51 -0300

    Person ViewModel structure

---
+++ AlexandreApps.Condominial.Backend/Model/AlexandreApps.Condominial.Backend.Model/Global/ViewModel/AddressViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlexandreApps.Condominial.Backend.Model.Global.ViewModel
+{
+    public class AddressViewModel
+    {
+        public Guid Id { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string ComplementaryInformation { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+    }
+}
---
+++ AlexandreApps.Condominial.Backend/Model/AlexandreApps.Condominial.Backend.Model/Global/ViewModel/PersonViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AlexandreApps.Condominial.Backend.Model.Global.ViewModel
+{
+    public class PersonViewModel
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public bool IsActive { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string DocumentCode { get; set; }
+        public string DocumentType { get; set; }
+        public string PreferedLanguage { get; set; }
+        public IList<AddressViewModel> Addresses { get; set; }
+        public IList<ContactViewModel> Contacts { get; set; }
+        public IList<NoteViewModel> Notes { get; set; }
+        public DateTime? LastUpdate { get; set; }
+    }
+}
